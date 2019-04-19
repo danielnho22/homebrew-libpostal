eb6828b1874f24c51d2bf1c4ba4b5f86dd7e7f45
Jay Zeschin <jay.zeschin@imagine-analytics.com>
2019-04-19 11:36:25 -0600

    Brew audit fixes

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -1,19 +1,21 @@
 class Libpostal < Formula
   desc "C library for parsing/normalizing street addresses around the world"
   homepage "https://github.com/openvenues/libpostal"
+
   url "https://github.com/openvenues/libpostal/archive/v1.0.0.tar.gz"
   sha256 "3035af7e15b2894069753975d953fa15a86d968103913dbf8ce4b8aa26231644"
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
+
+  head "https://github.com/openvenues/libpostal.git"
 
   devel do
     url "https://github.com/openvenues/libpostal/archive/v1.1-alpha.tar.gz"
     sha256 "c8a88eed70d8c09f68e1e69bcad35cb397e6ef11b3314e18a87b314c0a5b4e3a"
   end
 
-  head "https://github.com/openvenues/libpostal.git"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
     cpus = `sysctl -n hw.ncpu`.strip
@@ -25,27 +27,27 @@ class Libpostal < Formula
                           "--datadir=#{prefix}/data"
     system "make", "--jobs=#{cpus}"
     system "make", "install"
-    system "#{prefix}/bin/libpostal_data", "download", "all", "#{prefix}/data/libpostal"
+    system "#{bin}/libpostal_data", "download", "all", "#{prefix}/data/libpostal"
   end
-
+  
   test do
     (testpath/"test.c").write <<~EOS
-          #include <stdio.h>
-          #include <stdlib.h>
-          #include <libpostal/libpostal.h>
-          int main(int argc, char **argv) {
-              if (!libpostal_setup() || !libpostal_setup_parser()) {
-                  exit(EXIT_FAILURE);
-              }
-              libpostal_address_parser_options_t options = libpostal_get_address_parser_default_options();
-              libpostal_address_parser_response_t *parsed = libpostal_parse_address("781 Franklin Ave Crown Heights Brooklyn NYC NY 11216 USA", options);
-              libpostal_address_parser_response_destroy(parsed);
-              libpostal_teardown();
-              libpostal_teardown_parser();
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <libpostal/libpostal.h>
+      int main(int argc, char **argv) {
+          if (!libpostal_setup() || !libpostal_setup_parser()) {
+              exit(EXIT_FAILURE);
           }
-        EOS
+          libpostal_address_parser_options_t options = libpostal_get_address_parser_default_options();
+          libpostal_address_parser_response_t *parsed = libpostal_parse_address("781 Franklin Ave Crown Heights Brooklyn NYC NY 11216 USA", options);
+          libpostal_address_parser_response_destroy(parsed);
+          libpostal_teardown();
+          libpostal_teardown_parser();
+      }
+    EOS
 
-        system ENV.cc, "test.c", "-I#{prefix}/include", "-L#{prefix}/lib", "-lpostal", "-o", "test"
-        system "./test"
-      end
+    system ENV.cc, "test.c", "-I#{prefix}/include", "-L#{prefix}/lib", "-lpostal", "-o", "test"
+    system "./test"
+  end
 end
